54891c883ea2a55ae94e78fcf3a53d68de7f3a8c
Aiman Anizan <anizanaimann1@gmail.com>
2023-08-10 16:51:27 +0800

    Add custom font: Rubik

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        rubik: ['Rubik', 'sans-serif'],
+      },
       screens: {
         mobile: '375px',
         desktop: '1440px',
