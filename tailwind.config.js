032b9779a4b8fa094e42759b18bcd96f6d43eea6
Aiman Anizan <anizanaimann1@gmail.com>
2023-08-10 16:28:23 +0800

    Add custom configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        mobile: '375px',
+        desktop: '1440px',
+      },
+      colors: {
+        'dark-gray': 'hsl(var(--color-dark-gray)/<alpha-value>)',
+        'very-dark-gray': 'hsl(var(--color-very-dark-gray)/<alpha-value>)',
+      },
+    },
   },
   plugins: [],
 };
