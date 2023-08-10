c7afcdf56e7da69ce70ed847ce67efc012b0fcce
Aiman Anizan <anizanaimann1@gmail.com>
2023-08-10 17:55:48 +0800

    Add header and input box

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ArrowLeft from './assets/icon-arrow.svg';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main>
+        <div className="bg-background-mobile desktop:bg-background-desktop bg-no-repeat bg-cover relative">
+          <h1 className="text-3xl font-medium text-white max-w-xl text-center mx-auto pt-10 mb-4">IP Address Tracker</h1>
+
+          <div className="flex justify-center">
+            <input type="text" name="ip-address" id="ip-address" className="text-lg py-3 pl-5 rounded-l-xl min-w-[300px] w-[50vw] xl:w-[30vw] focus-visible:outline-none" placeholder="Search for any IP address or domain" />
+            <button className="bg-very-dark-gray min-w-[50px] rounded-r-xl flex align-middle justify-center">
+              <img src={ArrowLeft} alt="arrow left" className="my-5" />
+            </button>
+          </div>
+
+          <div>info</div>
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
