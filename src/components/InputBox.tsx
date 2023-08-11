9b263ed7222b1a587d455320f12938fbecc77055
Aiman Anizan <anizanaimann1@gmail.com>
2023-08-11 23:24:03 +0800

    Make code change: check domain or ip address that provided by user before make request

--- src/components/InputBox.tsx
+++ src/components/InputBox.tsx
@@ -1,5 +1,5 @@
 import { Dispatch, SetStateAction, useState } from 'react';
-import { exactUrl } from '../constant';
+import { domainRegex, exactUrl } from '../constant';
 import { ClientInfo } from '../types';
 import ArrowLeft from '../assets/icon-arrow.svg';
 
@@ -8,10 +8,12 @@ type Props = {
 };
 
 const InputBox = ({ setClientInfo }: Props) => {
-  const [IPAddress, setIPAddress] = useState<string>('');
+  const [inputValue, setInputValue] = useState<string>('');
 
   const getInfo = async () => {
-    const url = `${exactUrl}&ipAddress=${IPAddress}`;
+    let url: string;
+    if (domainRegex.test(inputValue)) url = `${exactUrl}&domain=${inputValue}`;
+    else url = `${exactUrl}&ipAddress=${inputValue}`;
     const response = await fetch(url);
     if (response.status != 200) {
       setClientInfo(null);
@@ -23,7 +25,7 @@ const InputBox = ({ setClientInfo }: Props) => {
 
   return (
     <div className="flex justify-center pb-40">
-      <input type="text" name="ip-address" id="ip-address" className="input-textbox" placeholder="Search for any IP address or domain" onChange={(event) => setIPAddress(event.target.value)} onKeyUp={(event) => event.key == 'Enter' && getInfo()} />
+      <input type="text" name="ip-address" id="ip-address" className="input-textbox" placeholder="Search for any IP address or domain" onChange={(event) => setInputValue(event.target.value)} onKeyUp={(event) => event.key == 'Enter' && getInfo()} />
       <button className="input-btn" onClick={() => getInfo()}>
         <img src={ArrowLeft} alt="arrow left" className="my-5" />
       </button>
