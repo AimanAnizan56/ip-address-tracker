b601d968d21f3fc071575966395a7fde13cf5efe
Aiman Anizan <anizanaimann1@gmail.com>
2023-08-11 23:23:28 +0800

    Add regex for domain

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -1,2 +1,3 @@
 const apiKey = `?apiKey=${import.meta.env.VITE_IPIFY_API_KEY}`;
 export const exactUrl = `https://geo.ipify.org/api/v2/country,city${apiKey}`;
+export const domainRegex = /([a-z0-9A-Z]\.)*[a-z0-9-]+\.([a-z0-9]{2,24})+(\.co\.([a-z0-9]{2,24})|\.([a-z0-9]{2,24}))*/g;
