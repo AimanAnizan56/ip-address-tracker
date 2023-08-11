bb99b00dfdb547cc33055eb5b4ff6cc86fb97daa
Aiman Anizan <anizanaimann1@gmail.com>
2023-08-11 17:15:26 +0800

    Move constant and types to seperate file

---
+++ src/constant/index.ts
@@ -0,0 +1,2 @@
+const apiKey = `?apiKey=${import.meta.env.VITE_IPIFY_API_KEY}`;
+export const exactUrl = `https://geo.ipify.org/api/v2/country,city${apiKey}`;
---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+export type ClientInfo = {
+  ip: string;
+  location: {
+    country: string;
+    region: string;
+    city: string;
+    lat: number;
+    lng: number;
+    postalCode: string;
+    timezone: string;
+    geonameId: number;
+  };
+  isp: string;
+};
