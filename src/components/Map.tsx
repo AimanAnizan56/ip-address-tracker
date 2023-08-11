d7c757ee1085a4567a19b7be464fbdddd6dcd0d6
Aiman Anizan <anizanaimann1@gmail.com>
2023-08-11 20:09:07 +0800

    Add fly to component to jump to current location

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,13 +1,24 @@
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, useMap } from 'react-leaflet';
 import locationIcon from '../assets/icon-location.svg';
 import 'leaflet/dist/leaflet.css';
 import { Icon } from 'leaflet';
+import { useEffect } from 'react';
 
 type Props = {
   location: {
     lat: number;
     lng: number;
   };
+};
+
+const FlyToComponent = ({ location }: Props) => {
+  const map = useMap();
+  useEffect(() => {
+    map.flyTo([location.lat, location.lng], map.getZoom(), {
+      duration: 2,
+    });
+  }, [location, map]);
+  return null;
 };
 
 const Map = ({ location }: Props) => {
@@ -18,9 +29,10 @@ const Map = ({ location }: Props) => {
   });
 
   return (
-    <MapContainer center={location} zoom={13} scrollWheelZoom={false} className="h-[66vh] w-[100vw] z-0">
+    <MapContainer center={location} zoom={16} scrollWheelZoom={false} className="h-[66vh] w-[100vw] z-0">
       <TileLayer attribution='<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>' url={url} />
       <Marker position={location} title="Current location" icon={customMarker} />
+      <FlyToComponent location={location} />
     </MapContainer>
   );
 };
